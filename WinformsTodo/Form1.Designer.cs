6fc34b29d240222851f8c589ff15601d7e450681
RedMan13 <godslayerakp@gmail.com>
2025-01-15 10:09:57 -0800

    i moved the button!!!!!!!!!!!!!!!!!!!!

--- WinformsTodo/Form1.Designer.cs
+++ WinformsTodo/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(188, 128);
+            button1.Location = new Point(273, 185);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
